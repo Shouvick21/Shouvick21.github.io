060052729ae501a47079bd3b4fbfa4a9f40a53e2
shouvick bachhar <bachharshouvickp2@gmail.com>
2024-07-28 18:10:28 +0530

    complete

--- index.js
+++ index.js
@@ -1,161 +1,139 @@
+let secton = document.querySelector("#sction");
+let menuicon = document.querySelector("#menuicon");
+let closeicon = document.querySelector("#closeicon");
 
-let secton=document.querySelector("#sction")
-let menuicon=document.querySelector("#menuicon")
-let closeicon=document.querySelector("#closeicon")
-
-menuicon.addEventListener("click",()=>{
-    console.log("yes")
-    menuicon.style.display="none"
-    closeicon.style.display="block"
-    secton.style.transform="translateX(0%)"
-    
-})
-closeicon.addEventListener("click",()=>{
-    menuicon.style.display="block"
-    closeicon.style.display="none"
-    secton.style.transform="translateX(100%)"
-    
-})
-let nav_link=document.querySelectorAll(".nav-link")
-nav_link.forEach((navlinkelement)=>{
-    navlinkelement.addEventListener("click",()=>{
-        if(window.innerWidth<="912"){
-            setTimeout(()=>{
-                menuicon.style.display="block"
-                closeicon.style.display="none"
-                secton.style.transform="translateX(100%)"
-            },500)
-        }
-    })
-
-})
+menuicon.addEventListener("click", () => {
+  console.log("yes");
+  menuicon.style.display = "none";
+  closeicon.style.display = "block";
+  secton.style.transform = "translateX(0%)";
+});
+closeicon.addEventListener("click", () => {
+  menuicon.style.display = "block";
+  closeicon.style.display = "none";
+  secton.style.transform = "translateX(100%)";
+});
+let nav_link = document.querySelectorAll(".nav-link");
+nav_link.forEach((navlinkelement) => {
+  navlinkelement.addEventListener("click", () => {
+    if (window.innerWidth <= "912") {
+      setTimeout(() => {
+        menuicon.style.display = "block";
+        closeicon.style.display = "none";
+        secton.style.transform = "translateX(100%)";
+      }, 500);
+    }
+  });
+});
 
 //--------------- darkmode and lightmode functionality-----------
 
-let tooglebox=document.querySelector("#tooglebox")
-let logo=document.querySelector("#logo")
-flag=true
-tooglebox.addEventListener("click",()=>{
-    if (flag){
-        logo.src="./images/navber/black logo.png"
-        document.body.classList.toggle("light_mode")
-        flag=false
-    }
-    else{
-        logo.src="./images/navber/white logo.png"
-        document.body.classList.toggle("light_mode")
-        flag=true
-    }
-    
-        
-
-    
-})
+let tooglebox = document.querySelector("#tooglebox");
+let logo = document.querySelector("#logo");
+flag = true;
+tooglebox.addEventListener("click", () => {
+  if (flag) {
+    logo.src = "./images/navber/black logo.png";
+    document.body.classList.toggle("light_mode");
+    flag = false;
+  } else {
+    logo.src = "./images/navber/white logo.png";
+    document.body.classList.toggle("light_mode");
+    flag = true;
+  }
+});
 //  ------------------------home section -------------------------
 
-let resume_navber=document.querySelector("#resume-link-1")
-let resume_btn =document.querySelector("#resume-button-2")
+let resume_navber = document.querySelector("#resume-link-1");
+let resume_btn = document.querySelector("#resume-button-2");
 
+const pdfopen_download = () => {
+  console.log("click");
+  pdfurl = "./ShouvickBachhar-FullStackWebDeveloper-Rtxg.pdf";
+  window.open(pdfurl, "_blank");
 
-const pdfopen_download=()=>{
-    console.log("click")
-    pdfurl="./ShouvickBachhar-FullStackWebDeveloper-Rtxg.pdf"
-    window.open(pdfurl,"_blank")
-    
-    let a_tag=document.createElement("a")
-    a_tag.href=pdfurl
-    a_tag.download=pdfurl
-    document.body.append("a_tag")
-    a_tag.click()
-    
+  let a_tag = document.createElement("a");
+  a_tag.href = pdfurl;
+  a_tag.download = pdfurl;
+  document.body.append("a_tag");
+  a_tag.click();
+};
+resume_btn.addEventListener("click", pdfopen_download);
+const pdfdownload = () => {
+  pdfurl = "./ShouvickBachhar-FullStackWebDeveloper-Rtxg.pdf";
 
-}
-resume_btn.addEventListener("click",pdfopen_download)
-const pdfdownload=()=>{
-    
-    pdfurl="./ShouvickBachhar-FullStackWebDeveloper-Rtxg.pdf"
-    
-    let a_tag=document.createElement("a")
-    a_tag.href=pdfurl
-    a_tag.download=pdfurl
-    document.body.append("a_tag")
-    a_tag.click()
-    
-    
+  let a_tag = document.createElement("a");
+  a_tag.href = pdfurl;
+  a_tag.download = pdfurl;
+  document.body.append("a_tag");
+  a_tag.click();
+};
+resume_navber.addEventListener("click", pdfdownload);
 
-}
-resume_navber.addEventListener("click",pdfdownload)
+let contact_github1 = document.querySelector("#contact-github1");
 
+contact_github1.addEventListener("click", () => {
+  window.open("https://github.com/Shouvick21", "_blank");
+});
+let contact_linkedin1 = document.querySelector("#contact-linkedin1");
+contact_linkedin1.addEventListener("click", () => {
+  window.open("https://www.linkedin.com/in/shouvickbachhar/", "_blank");
+});
 
-let contact_github1=document.querySelector("#contact-github1")
+let contact_twitter = document.querySelector("#contact-twitter");
+contact_twitter.addEventListener("click", () => {
+  window.open("https://x.com/BachharShouvick", "_blank");
+});
 
-contact_github1.addEventListener("click",()=>{
-    window.open("https://github.com/Shouvick21","_blank")
-})
-let contact_linkedin1=document.querySelector("#contact-linkedin1")
-contact_linkedin1.addEventListener("click",()=>{
-    window.open("https://www.linkedin.com/in/shouvickbachhar/","_blank")
-})
-
-let contact_twitter=document.querySelector("#contact-twitter")
-contact_twitter.addEventListener("click",()=>{
-    window.open("https://x.com/BachharShouvick","_blank")
-})
-
-
-let contact_facebook=document.querySelector("#contact-facebook").addEventListener("click",()=>{
-    window.open("https://www.facebook.com/shouvick.bachhar","_blank")
-})
+let contact_facebook = document
+  .querySelector("#contact-facebook")
+  .addEventListener("click", () => {
+    window.open("https://www.facebook.com/shouvick.bachhar", "_blank");
+  });
 
 // ------------------------project section--------------------
 
 // -------------Toogood project-------------------------
-let toogood_github=document.querySelector("#toogood_github")
-let toogood_deploy=document.querySelector("#toogood_deploy")
+let toogood_github = document.querySelector("#toogood_github");
+let toogood_deploy = document.querySelector("#toogood_deploy");
 
-toogood_github.addEventListener("click",()=>{
-    window.open("https://github.com/Shouvick21/toogood","_blank")
-})
-toogood_deploy.addEventListener("click",()=>{
-    window.open("https://toogood-ten.vercel.app/","_blank")
-})
+toogood_github.addEventListener("click", () => {
+  window.open("https://github.com/Shouvick21/toogood", "_blank");
+});
+toogood_deploy.addEventListener("click", () => {
+  window.open("https://toogood-ten.vercel.app/", "_blank");
+});
 
 //  ------------scheduler project-----------------------------
-let Scheduler_github=document.querySelector("#Scheduler_github")
-let Scheduler_deploy=document.querySelector("#Scheduler_deploy")
+let Scheduler_github = document.querySelector("#Scheduler_github");
+let Scheduler_deploy = document.querySelector("#Scheduler_deploy");
 
-Scheduler_github.addEventListener("click",()=>{
-    window.open("https://github.com/Shouvick21/Scheduler","_blank")
-})
-Scheduler_deploy.addEventListener("click",()=>{
-    window.open("https://scheduler-sable.vercel.app/","_blank")
-})
-
-
+Scheduler_github.addEventListener("click", () => {
+  window.open("https://github.com/Shouvick21/Scheduler", "_blank");
+});
+Scheduler_deploy.addEventListener("click", () => {
+  window.open("https://scheduler-sable.vercel.app/", "_blank");
+});
 
 // -----------------contact section-----------------
 
-let contact_github=document.querySelector("#contact-github")
+let contact_github = document.querySelector("#contact-github");
 
-contact_github.addEventListener("click",()=>{
-    window.open("https://github.com/Shouvick21","_blank")
-})
-let contact_linkedin=document.querySelector("#contact-linkedin")
-contact_linkedin.addEventListener("click",()=>{
-    window.open("https://www.linkedin.com/in/shouvickbachhar/","_blank")
-})
+contact_github.addEventListener("click", () => {
+  window.open("https://github.com/Shouvick21", "_blank");
+});
+let contact_linkedin = document.querySelector("#contact-linkedin");
+contact_linkedin.addEventListener("click", () => {
+  window.open("https://www.linkedin.com/in/shouvickbachhar/", "_blank");
+});
 
-let phno=document.querySelector("#contact-phone")
-phno.addEventListener("click",()=>{
-    window.open("tel:"+"+916289858863","_blank")
-})
+let phno = document.querySelector("#contact-phone");
+phno.addEventListener("click", () => {
+  window.open("tel:" + "+916289858863", "_blank");
+});
 
+let email = document.querySelector("#contact-email");
+email.addEventListener("click", () => {
+  window.open("mailto:" + "bachharshouvickp2@gmail.com", "_blank");
+});
 
-let email=document.querySelector("#contact-email")
-email.addEventListener("click",()=>{  
-    window.open("mailto:"+"bachharshouvickp2@gmail.com","_blank")
-})
-// let submit=document.querySelector("#submit")
-// submit.addEventListener("click",()=>{
-//     window.open("index.html","_blank")
-// })
